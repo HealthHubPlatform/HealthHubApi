2a5a7988710dd2177515a3f0ca8915b5609e595b
Kuldeep Chowhan <kchowhan@sofi.org>
2020-08-27 01:24:40 -0500

    updated rewrite rules

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,29 @@ import errorHandler from "errorhandler";
 import app from "./app";
 import logger from "./util/logger";
 
+import { ApolloServer } from "apollo-server-express";
+
+import { typeDefs, resolvers } from "./schema";
+
 /**
  * Error Handler. Provides full stack - remove for production
  */
 app.use(errorHandler());
 
 /**
- * Start Express server.
+ * Start Express Apollo server.
  */
-const server = app.listen(app.get("port"), () => {
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    logger: logger,
+    playground: true,
+    introspection: true,
+});
+
+server.applyMiddleware({ app });
+
+app.listen(app.get("port"), () => {
     console.log(
         "  App is running at http://localhost:%d in %s mode",
         app.get("port"),
